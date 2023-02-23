23f7b89092446cc6ad34fbd10ab337fb9b5964b2
JavGog <javierggjavier@gmail.com>
2023-02-23 21:46:34 +0000

    (Q3) (h) Updating printBarChart method.

--- src/main/java/PopulationModel.java
+++ src/main/java/PopulationModel.java
@@ -11,11 +11,10 @@ public class PopulationModel {
     private static final double KILL_RATEA = 0.2;
     private static final double KILL_RATEB = 0.1;
     private static final int NUM_WEEKS = 10;
-    private static int numAnext = INITIALA;
-    private static int numBnext = INITIALB;
 
     //(Q3)(c)
-    public PopulationModel() {
+    public PopulationModel()
+    {
         numA = new int[NUM_WEEKS];
         numB = new int[NUM_WEEKS];
         numA[0] = INITIALA;
@@ -23,11 +22,11 @@ public class PopulationModel {
     }
 
     //(Q3) (d)
-    public int newNumA(int currentA, int currentB) {
+    public int newNumA(int currentA, int currentB)
+    {
         int newNumA;
         newNumA = (int) Math.floor(currentA - KILL_RATEB * currentB);
         if (newNumA >= 0) {
-            numAnext = newNumA;
             return newNumA;
         } else {
             return 0;
@@ -35,11 +34,11 @@ public class PopulationModel {
     }
 
     //(Q3) (e)
-    public int newNumB(int currentA, int currentB) {
+    public int newNumB(int currentA, int currentB)
+    {
         int newNumB;
         newNumB = (int) Math.floor(currentB - KILL_RATEA * currentA);
         if (newNumB >= 0) {
-            numBnext = newNumB;
             return newNumB;
         } else {
             return 0;
@@ -47,28 +46,38 @@ public class PopulationModel {
     }
 
     //(Q3) (f)
-    public void storeNewPopulations(int thisWeek) {
-
-        int nextA = newNumA(numAnext, numBnext);
+    public void storeNewPopulations(int thisWeek)
+    {
+        int nextA = newNumA(numA[thisWeek-1], numA[thisWeek-1]);
         numA[thisWeek] = nextA;
-        int nextB = newNumB(numAnext, numBnext);
+        int nextB = newNumB(numB[thisWeek-1], numB[thisWeek-1]);
         numB[thisWeek] = nextB;
     }
 
     //(Q3) (g)
-    public void createData() {
+    public void createData()
+    {
         for (int i = 1; i < NUM_WEEKS; i++) {
             storeNewPopulations(i);
         }
     }
 
     //(Q3) (h)
-    public void printBarChart() {
-        int x = 0;
-        for (int num : numA) {
-            System.out.println("Week " + x + "A" + "*".repeat(numA[x]) + numA[x]);
-            System.out.println("Week " + x + "B" + "*".repeat(numB[x]) + numB[x]);
-            x++;
+    public void printBarChart()
+    {
+        StringBuilder builder;
+        StringBuilder builder2;
+        for (int n = 0 ; n < numA.length; n++) {
+            builder = new StringBuilder(numA[n]);
+            builder2 = new StringBuilder(numB[n]);
+            for (int i = 0; i < numA[n]; i++) {
+                builder.append("*");
+            }
+            for (int i = 0; i < numB[n]; i++) {
+                builder2.append("*");
+            }
+            System.out.println("Week " + n + "A" + builder.toString() + numA[n]);
+            System.out.println("Week " + n + "B" + builder2.toString() + numB[n]);
         }
     }
 
